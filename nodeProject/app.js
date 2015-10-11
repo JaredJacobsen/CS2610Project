58d9487762437e43e83fd8f406ed8275fc250210
JaredJacobsen <jaredtjacobsen@gmail.com>
2015-10-11 17:10:43 -0600

    Moved content from old project structure into new project structure

--- nodeProject/app.js
+++ nodeProject/app.js
@@ -1,12 +1,23 @@
 var express 	= require('express')
 	, exphbs		= require('express-handlebars')
+	, path      = require('path')
   , port      = 3000
 	, router 	= express.Router();
 
 var app = express();
 
-app.engine('handlebars', exphbs({defaultLayout: 'base'}));
+app.engine('handlebars', exphbs({defaultLayout: 'auth_base'}));
 app.set('view engine', 'handlebars');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res) {
+  res.render('index', {
+    title: 'This is a title!',
+    welcome: 'Welcome to the site!'
+  })
+})
+
 
 app.get('/profile', function(req, res) {
   res.render('profile', {
