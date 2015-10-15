dd2b47c06f0eaf5fca11e920f39b68199c64a8bb
JaredJacobsen <jaredtjacobsen@gmail.com>
2015-10-14 19:34:33 -0600

    Changed auth_base to allow for multiple css files

--- nodeProject/routes/userRoutes/userRoutes.js
+++ nodeProject/routes/userRoutes/userRoutes.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 router.get('/dashboard', function(req, res) {
   res.render('dashboard', {
-
+    title: 'Welcome to the dashboard',
+    css: "/css/style.css"
   })
 })
 
