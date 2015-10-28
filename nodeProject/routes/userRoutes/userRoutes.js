1a5873d0b3e04fc47868bd4dcc3fb81e966ec1ae
JaredJacobsen <jaredtjacobsen@gmail.com>
2015-10-28 16:30:25 -0600

    Worked on moving to handlebars and node

--- nodeProject/routes/userRoutes/userRoutes.js
+++ nodeProject/routes/userRoutes/userRoutes.js
@@ -3,14 +3,27 @@ var router = express.Router();
 
 router.get('/dashboard', function(req, res) {
   res.render('dashboard', {
-    title: 'Welcome to the dashboard',
-    css: "/css/style.css"
+    title: 'dashboard',
+    css: "/css/JaredJ.css"
   })
 })
 
 router.get('/profile', function(req, res) {
   res.render('profile', {
+    title: "User Profile"
+  })
+})
 
+router.get('/savedSearches', function(req, res) {
+  res.render('savedSearches', {
+    title: "Saved Searches"
+  })
+})
+
+router.get('/search', function(req, res) {
+  res.render('search', {
+    title: 'search',
+    css: "/css/MichaelK.css"
   })
 })
 module.exports = router
