9da1441c8058cdcb8b5f741e2183e9758d136b13
JaredJacobsen <jaredtjacobsen@gmail.com>
2015-11-11 09:39:47 -0700

    added username to nav bar

--- nodeProject/app.js
+++ nodeProject/app.js
@@ -57,7 +57,8 @@ app.get('/auth/finalize', function(req, res) {
   request.post(options, function(error, response, body) {
     var data = JSON.parse(body)
     req.session.access_token = data.access_token
-    console.log(data)
+		req.session.username = data.user.username
+    //console.log(data)
     res.redirect('/user/dashboard')
   })
 })
